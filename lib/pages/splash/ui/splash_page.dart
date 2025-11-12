bbc2d5f17d585ea953968d4995971faa1479af55
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-12 00:30:03 -0500

    feat(splash_page): refactor to use StatelessWidget and initialize token validation in build method
    Convert SplashPage from StatefulWidget to StatelessWidget and move token validation logic to the build method for improved performance and simplicity.

--- lib/pages/splash/ui/splash_page.dart
+++ lib/pages/splash/ui/splash_page.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:waste_track_driver_app/app/di/injection_container.dart';
 import 'package:waste_track_driver_app/app/theme/app_colors.dart';
 import 'package:waste_track_driver_app/features/auth/auth.dart';
 
-class SplashPage extends StatefulWidget {
+class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-    // Validate token on startup
-    sl<AuthBloc>().add(const TokenValidationRequested());
-  }
-
-  @override
   Widget build(BuildContext context) {
+    context.read<AuthBloc>().add(const TokenValidationRequested());
+
     return BlocListener<AuthBloc, AuthState>(
-      bloc: sl<AuthBloc>(),
       listener: (context, state) {
         // Browse by status
         if (state is AuthAuthenticated) {
@@ -39,7 +27,6 @@ class _SplashPageState extends State<SplashPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // Logo
-              // TODO: Change logo with ours
               Container(
                 width: 120,
                 height: 120,
@@ -56,9 +43,9 @@ class _SplashPageState extends State<SplashPage> {
 
               const SizedBox(height: 32),
 
-              // Name of the app
+              // Nombre de la app
               const Text(
-                'Waste Track',
+                'EcoLutions',
                 style: TextStyle(
                   fontSize: 32,
                   fontWeight: FontWeight.bold,
@@ -85,7 +72,6 @@ class _SplashPageState extends State<SplashPage> {
               const SizedBox(height: 16),
 
               BlocBuilder<AuthBloc, AuthState>(
-                bloc: sl<AuthBloc>(),
                 builder: (context, state) {
                   return Text(
                     state is AuthValidating
