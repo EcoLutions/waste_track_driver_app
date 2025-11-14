a6f7004cb02e2f31b7fe50a9d299dd899c77503f
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-14 12:17:20 -0500

    feat(auth): create authentication module exports
    Add exports for authentication API, models, and UI components to streamline imports and enhance module organization.

---
+++ lib/features/authentication/authentication.dart
@@ -0,0 +1,11 @@
+// API
+export 'api/auth_repository.dart';
+export 'api/auth_repository_impl.dart';
+export 'api/auth_service.dart';
+// Model
+export 'model/authenticated_user_response.dart';
+export 'model/sign_in_request.dart';
+
+// UI
+export 'ui/login_form.dart';
+export 'ui/login_header.dart';
