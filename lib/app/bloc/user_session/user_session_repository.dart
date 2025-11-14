36119111eaafde0d949686c5568cb4820163070e
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-14 12:14:33 -0500

    feat(auth): implement user session repository and its implementation
    Add UserSessionRepository interface and UserSessionRepositoryImpl class to manage user session data, including user, user profile, and district loading functionalities.

---
+++ lib/app/bloc/user_session/user_session_repository.dart
@@ -0,0 +1,23 @@
+import 'package:waste_track_driver_app/entities/district/district.dart';
+import 'package:waste_track_driver_app/entities/user/user.dart';
+import 'package:waste_track_driver_app/entities/user_profile/user_profile.dart';
+import 'package:waste_track_driver_app/shared/lib/utils/resource.dart';
+
+abstract class UserSessionRepository {
+  Future<Resource<User>> loadUser(String userId);
+  Future<Resource<UserProfile>> loadUserProfile(String userId);
+  Future<Resource<District>> loadDistrict(String districtId);
+  Future<Resource<UserSessionData>> loadCompleteSession(String userId);
+}
+
+class UserSessionData {
+  const UserSessionData({
+    required this.user,
+    required this.userProfile,
+    this.district,
+  });
+
+  final User user;
+  final UserProfile userProfile;
+  final District? district;
+}
